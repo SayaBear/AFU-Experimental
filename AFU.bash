875c99ed1ca93daef3794618d8f7161ac6e8db7a
SayaBear <39145906+SayaBear@users.noreply.github.com>
2018-08-21 11:03:31 +0800

    Update AFU.bash

--- AFU.bash
+++ AFU.bash
@@ -13,8 +13,8 @@ sudo apt install unzip
 sudo apt install screen
 wget https://github.com/JustArchi/ArchiSteamFarm/releases/download/3.3.0.3/ASF-linux-x64.zip
 unzip ASF-linux-x64.zip -d ASF/
-echo -e "请选择您是需要单纯挂卡还是需要进行进阶操作 \n1.单纯挂卡 \n2.进阶操作"
-read -r -p "请输入数字" IL
+echo -e "请选择您是需要单纯挂卡还是需要进行进阶操作 \n1.单纯挂卡 \n2.进阶操作 \n3.请输入数字";
+read IL
 case $IL in
   1)cd ASF/config
     touch bot.json
@@ -25,13 +25,16 @@ case $IL in
     "\"IsBotAccount\"": false
   }" > /root/ASF/config/bot.json
   ;;
-  2)echo -e "是否需要开启通过steam组输入指令功能？ \n1.开启 \n2.不开启"
+  2)echo -e "是否需要开启通过steam组输入指令功能？ \n1.开启 \n2.不开启";
     read -r -p "请输入数字" GP
     case $GP in
     1)echo "输入你的steam64位id"
       read ID
       echo "输入你的组id"
       read Group
+	  echo -e "请选择您期待的挂卡顺序 \n0.无序模式 \n1.根据AppID正序挂卡 \n2.根据AppID逆序挂卡 \n3.根据剩余掉卡数自少到多挂卡 \n4.根据剩余挂卡数自多到少挂卡 \n5.根据游戏时间自少到多挂卡 \n6.根据游戏时间自多到少挂卡 \n7.根据游戏名A-Z挂卡 \n8.根据游戏名Z-A挂卡 \n9.随机模式 \n10.根据徽章等级由低到高挂卡 \n11.根据徽章等级由高到低挂卡 \n12.根据游戏入库时间由远及近挂卡 \n13.根据游戏入库时间由近及远挂卡 \n14.优先挂不可交易卡牌 \n15.优先挂可交易卡牌"
+	  read Order
+	  echo -e "
       cd ASF/config
       touch bot.json
       echo -e "{
